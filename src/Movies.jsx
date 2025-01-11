aee7fb51590d78d5d553f0ac42822a2b48164633
ayushi <you@example.com>
2025-01-11 10:41:50 -0800

    cards

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -1,24 +1,28 @@
 import React, { useContext } from "react";
 import { AppContext } from "./context";
 import { Link } from "react-router-dom";
-import Home from "./Home";
 
 function Movies() {
   const { movie } = useContext(AppContext);
 
   return (
-    <>
+    <div className="movieTv-grid">
       {movie?.map((data) => (
-        <Link to={`/Movies/MovieDetail/${data.id}`} key={data.id}>
+        <Link
+          to={`/Movies/MovieDetail/${data.id}`}
+          key={data.id}
+          className="movieTv-card"
+        >
           <img
             key={data.id}
             src={`https://image.tmdb.org/t/p/w500${data?.poster_path}`}
             alt={data.title || "movie poster"}
+            className="movieTv-poster"
           />
-          <h2>{data.title}</h2>
+          <h2 className="movieTv-title">{data.title}</h2>
         </Link>
       ))}
-    </>
+    </div>
   );
 }
 
--- src/PageNotFound.jsx
+++ src/PageNotFound.jsx
@@ -4,12 +4,19 @@ import { useNavigate } from "react-router-dom";
 function PageNotFound() {
   const navigate = useNavigate();
   return (
-    <>
-      <div>
-        <button onClick={() => navigate("/Movies")}>Back</button>
+    <div className="home-background">
+      <div className="App-header">
+        <div className="content-wrapper">
+          <div className="back-button">
+            <button onClick={() => navigate("/Movies")}>Back</button>
+          </div>
+          <div className="error-message">
+            ERROR <br />
+            404 Page Not Found !!
+          </div>
+        </div>
       </div>
-      <div> 404 PageNotFound </div>
-    </>
+    </div>
   );
 }
 
